644f8e27856d16287f21a4059aead8f789869a6d
jedrzejjarocki <jedrzej.jarocki@gmail.com>
2020-07-24 16:20:56 +0200

    backlog creation bug fixed

--- src/main/js/redux/issuesContainers/reducer.ts
+++ src/main/js/redux/issuesContainers/reducer.ts
@@ -1,8 +1,10 @@
-import {Map, updateIn} from 'immutable';
-import {RootAction} from '../rootReducer';
-import {ADD_SPRINT, DELETE_SPRINT, IssuesContainersState, SET_ISSUES_CONTAINERS, UPDATE_SPRINT,} from './types';
-import {ADD_ISSUE, DELETE_ISSUE} from '../issues/types';
-import {ADD_PROJECT} from '../projects/types';
+import { Map, updateIn } from 'immutable';
+import { RootAction } from '../rootReducer';
+import { ADD_SPRINT, DELETE_SPRINT, IssuesContainersState, SET_ISSUES_CONTAINERS, UPDATE_SPRINT, } from './types';
+import { ADD_ISSUE, DELETE_ISSUE } from '../issues/types';
+import { ADD_PROJECT } from '../projects/types';
+import Backlog from '../../entities/Backlog';
+import { IssuesContainer } from '../../entities/IssuesContainer';
 
 export default (state: IssuesContainersState = Map(), action: RootAction) => {
   switch (action.type) {
@@ -34,8 +36,8 @@ export default (state: IssuesContainersState = Map(), action: RootAction) => {
     }
 
     case ADD_PROJECT: {
-      const { backlog } = action.payload;
-      return state.set(`${backlog.id}`, backlog);
+      const { id } = action.payload.backlog;
+      return state.set(`${id}`, new Backlog({ id }) as IssuesContainer);
     }
 
     default:
